c9abaa8ecc3f35d089b56c4d1f46c33390dd3450
정오 <aidenchung5312@gmail.com:git config --global user.email aidenchung5312@gmail.comclarclearqcvgrtfzadftgg>
2019-08-20 00:34:45 +0900

    SYNC

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,14 @@
 // var stringifyJSON = JSON.stringify;
 
 // 하지만 이 과제의 목적은 재귀를 공부하는 것이니, 처음부터 구현해봐야겠지요?:
+
+/**pseudo code
+ * 
+ * -JSON.stringify와 같은 기능을 하는 함수를 재귀로 구현.
+ * 
+ *  -obj를 입력
+ * 
+ */
 function stringifyJSON(obj) {
   // your code goes here
 };

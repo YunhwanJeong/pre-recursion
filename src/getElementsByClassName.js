4d66315e286f6e9f11305a93c3b39ab2af7da192
Yun Hwan Chung <yun_hwan_chung@bat.com>
2019-08-21 17:57:42 +0900

    SYNC

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,13 @@
 // };
 
 // 하지만 이 과제의 목적은 재귀를 공부하는 것이니, 처음부터 구현해봐야겠지요?:
+
+/**pseudo code
+ * 
+ * 인자로 className을 받아서 -> 호출한 root Element의 모든 자식 요소들의 class들과 대조하여 -> 일치하는 요소들 
+ * 
+ * 
+ */
 function getElementsByClassName(className) {
-  // your code here
+
 };
